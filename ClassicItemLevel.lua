8b6ffe68139f619d308deef7fa1c3a775a9bb948
Pawaox <pawaox@gmail.com>
2025-10-22 19:43:58 +0200

    Reworked

--- ClassicItemLevel.lua
+++ ClassicItemLevel.lua
@@ -1,4 +1,4 @@
-LWItemlevelSettings=LWItemlevelSettings or {}
+SV_ilvl=SV_ilvl or {}
 local sEnabled=1
 local sEquipOnly=2
 local sColor=3
@@ -31,19 +31,19 @@ local function GetItemLevelAndEquip(itemLink)
     return iLevel or -1,iRarity or 1,(iEquipLoc~=nil and iEquipLoc~="")
 end
 local function EventItem(itemLink,tooltipObj)
-	if LWItemlevelSettings[sEnabled]==false then
+	if SV_ilvl[sEnabled]==false then
 		return
 	end
 
 	local ilvl,iRarity,isEquipItem=GetItemLevelAndEquip(itemLink)
-	local showEquipOnly=LWItemlevelSettings[sEquipOnly]
+	local showEquipOnly=SV_ilvl[sEquipOnly]
 	if showEquipOnly==nil then
 		showEquipOnly=true
 	end
 	if (showEquipOnly and isEquipItem) or not showEquipOnly then
 		if ilvl and ilvl>=0 then
 			local color="|cFFFFFFFF"
-			if LWItemlevelSettings[sColor]~=false then
+			if SV_ilvl[sColor]~=false then
 				color=GetItemColor(iRarity)
 			end
 			tooltipObj:AddLine("ItemLevel: "..color..ilvl.."|r")
@@ -52,15 +52,15 @@ local function EventItem(itemLink,tooltipObj)
 end
 local function SetSettingNotify(setting,value,txtBase,txtOn,txtOff,txtEnabled,txtDisabled,ensureAddonOn,elseCheck)
 	if (value=="on" or value=="enable" or value=="yes" or value=="y") then
-		LWItemlevelSettings[setting]=true
-		if ensureAddonOn then LWItemlevelSettings[sEnabled]=true end
+		SV_ilvl[setting]=true
+		if ensureAddonOn then SV_ilvl[sEnabled]=true end
 		NotifyUser(txtBase..txtOn)
 	elseif (value=="off" or value=="disable" or value=="no" or value=="n") then
-		LWItemlevelSettings[setting]=false
-		if ensureAddonOn then LWItemlevelSettings[sEnabled]=true end
+		SV_ilvl[setting]=false
+		if ensureAddonOn then SV_ilvl[sEnabled]=true end
 		NotifyUser(txtBase..txtOff)
 	elseif elseCheck==true then
-		if LWItemlevelSettings[setting]==nil and true or LWItemlevelSettings[setting] then
+		if SV_ilvl[setting]==nil and true or SV_ilvl[setting] then
 			NotifyUser(txtBase..txtEnabled)
 		else
 			NotifyUser(txtBase..txtDisabled)
@@ -85,7 +85,7 @@ SlashCmdList["CITEMLEVEL"]=function(msg)
 		end
 		if p1=="equip" then
 			SetSettingNotify(sEquipOnly,p2,"'EquipOnly' ","on","off","is on","is off",true,true)
-		elseif p1=="color" or p1=="colour" then
+		elseif p1=="color" or p1=="colour" or p1=="colors" or p1=="colours" then
 			SetSettingNotify(sColor,p2,"Colors ","on","off","are on","are off",true,true)
 		else
 			SetSettingNotify(sEnabled,p1,"Addon ","on","off",nil,nil,false,false)
@@ -97,7 +97,7 @@ local function EventAddonLoaded(self, event, addonName)
 	if addonName=="ClassicItemLevel" then
 		local defaults={[sEnabled]=true, [sEquipOnly]=true, [sColor]=true}
 		for k,v in pairs(defaults) do
-			if LWItemlevelSettings[k]==nil then LWItemlevelSettings[k]=v end
+			if SV_ilvl[k]==nil then SV_ilvl[k]=v end
 		end
 		basicEventFrame:UnregisterEvent("ADDON_LOADED")
 	end
